af18bace45a46b487755d2d1dde26e69ce90f0a1
Kaisersoft.ai <kaiserbox.uk@gmail.com>
2026-09-21 08:30:37 +0200

    Fix current frontend build metadata

--- build-info.js
+++ build-info.js
@@ -1,5 +1,5 @@
 window.MEMELAB_BUILD = {
   version: "V0.01",
-  build: "$"+"{GITHUB_RUN_NUMBER}",
-  builtAt: "$"+"(date -u +'%Y-%m-%dT%H:%M:%SZ')"
+  build: "manual",
+  builtAt: "2026-09-21T06:30:35.051Z"
 };
